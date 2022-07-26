bd23e54f8720f338e0edc4ea5db0401347b0cc61
malvernbright <malvernbright.95@gmail.com>
2022-07-26 14:42:27 +0200

    Added separate css and js folder

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
+let slideId = "mySlides";
 showSlides(1, 0);
 showSlides(1, 1);
 
@@ -9,7 +9,7 @@ function plusSlides(n, no) {
 
 function showSlides(n, no) {
   let i;
-  let x = document.getElementsByClassName(slideId[no]);
+  let x = document.getElementsByClassName(slideId);
   if (n > x.length) {slideIndex[no] = 1}    
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
